56d48abb30348338157469d2139710553c995b0f
“Slava <vanavara06908@gmail.com>
2023-01-07 18:07:26 +0300

    Added new parts

--- dental/website/views.py
+++ dental/website/views.py
@@ -21,3 +21,43 @@ def contact(request):
     else:
         #return the page
         return render(request, 'contact.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+# def about(request):
+#     if request.method == 'POST':
+#         #do stuff
+#         message_name = request.POST['message-name']
+#         message_email = request.POST['message-email']
+#         message = request.POST['message']
+#
+#         send_mail(
+#             message_name, #subject
+#             message, #message
+#             message_email, #from email
+#             ['vanavara1986@gmail.com'], #to email
+#             fail_silently = False,
+#         )
+#         return render(request, 'about.html', {'message_name': message_name})
+#     else:
+#         #return the page
+#         return render(request, 'about.html', {})
+
+
+def service(request):
+    return render(request, 'service.html', {})
+
+
+def blog(request):
+    return render(request, 'blog.html', {})
+
+
+def pricing(request):
+    return render(request, 'pricing.html', {})
+
+
+def blog_details(request):
+    return render(request, 'blog_details.html', {})
